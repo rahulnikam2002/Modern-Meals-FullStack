55fa0dceab61273f8eee12568c2428c4796b8ffb
Rahul Nikam <codewithrahulnikam@gmail.com>
2022-10-02 13:05:16 +0530

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { HomeScreen } from './Screens/Home/Home.Screen';
+
+function App() {
+  return (
+    <div className="App">
+      <HomeScreen />
+    </div>
+  );
+}
+
+export default App;
